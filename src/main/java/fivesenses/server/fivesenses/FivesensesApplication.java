30607a6e1e784b0c27b85d832d7c8ecbfab7098d
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-07-26 21:18:49 +0900

    Initial commit

---
+++ src/main/java/fivesenses/server/fivesenses/FivesensesApplication.java
@@ -0,0 +1,13 @@
+package fivesenses.server.fivesenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivesensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivesensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fivesenses/server/fivesenses/FivesensesApplicationTests.java
@@ -0,0 +1,13 @@
+package fivesenses.server.fivesenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivesensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
