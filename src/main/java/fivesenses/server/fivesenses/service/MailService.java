7958d2b52e79699d239aee9ef875d22af85cd688
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-08-05 23:20:44 +0900

    Feat: 추가 메일링 관련 기본 설정

--- src/main/java/fivesenses/server/fivesenses/service/MailService.java
+++ src/main/java/fivesenses/server/fivesenses/service/MailService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MailService {
     private final JavaMailSender mailSender;
-    private static final String FROM_ADDRESS = "ourpageapp@gmail.com";
+    private static final String FROM_ADDRESS = "hi.mangpo@gmail.com";
 
     public void lostPw(String userEmail, String randomPw) {
         SimpleMailMessage message = new SimpleMailMessage();
