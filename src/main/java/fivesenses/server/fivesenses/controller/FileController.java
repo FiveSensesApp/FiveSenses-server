6b4739343edbbb6fac842901340d6292c65b2218
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-09-13 23:44:00 +0900

    Feat: 추가 파일 관련 기능

---
+++ src/main/java/fivesenses/server/fivesenses/controller/FileController.java
@@ -0,0 +1,43 @@
+package fivesenses.server.fivesenses.controller;
+
+import fivesenses.server.fivesenses.dto.Meta;
+import fivesenses.server.fivesenses.dto.Result;
+import fivesenses.server.fivesenses.service.S3Service;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    private final S3Service s3Service;
+
+    @PutMapping("/upload")
+    public ResponseEntity<Result<String>> upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+        String imgLocation = s3Service.upload(multipartFile);
+
+        Result<String> result = new Result<>(new Meta(HttpStatus.OK.value()), imgLocation);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam String imageLocation){
+        String[] s = imageLocation.split("/");
+        String fileName = s[s.length - 1];
+
+        s3Service.deleteFile(fileName);
+
+        Result<?> result = new Result<>(new Meta(HttpStatus.OK.value()));
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/fivesenses/server/fivesenses/service/S3Service.java
@@ -0,0 +1,79 @@
+package fivesenses.server.fivesenses.service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class S3Service {
+
+    private final static String DIR_NAME = "images/";
+
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        StringBuffer sb = new StringBuffer();
+
+        String fileName = sb.append(DIR_NAME)
+                .append(UUID.randomUUID().toString())
+                .append("_")
+                .append(file.getOriginalFilename())
+                .toString();
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
+
+    public void deleteFile(String fileName) {
+        String path = DIR_NAME + fileName;
+
+        s3Client.deleteObject(bucket, path);
+    }
+
+}
